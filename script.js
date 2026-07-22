8c963cbc3009bc84939c761f5a243c5d5eb2d8db
Kelwin Apresdio Da silva <kapresidio@gmail.com>
2026-07-22 14:00:34 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 // Script simples para abrir/fechar menu hambúrguer
 const toggle = document.getElementById("menu-toggle");
 const links = document.getElementById("nav-links");
+const linkButtons = document.querySelectorAll("button[data-url]");
  
  
 // Adiciona um evento de clique ao botão
 toggle.addEventListener("click", () => {
     links.classList.toggle("active");
+});
+
+linkButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+        const url = button.getAttribute("data-url");
+        if (url) {
+            window.open(url, "_blank", "noopener");
+        }
+    });
 });
  
